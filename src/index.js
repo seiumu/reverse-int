7bc968588fb9c7ccd6320f7ad3a33d2ea9a955da
Serghei Musaji <serghei.musaji@gmail.com>
2022-01-06 00:04:19 -0400

    doc: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  var reversed = "";
+  var numtext = n.toString();
+  var digits = n.toString().length;
+  for(i=0; i<digits; i++){
+    if(numtext[i]==="-"){
+      current = "";
+    }
+    else{
+      current = numtext[i];
+    }
+    reversed = current + reversed;
+  }
+  console.log(reversed);
+  return reversed;
 }
